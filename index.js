b2e67bda0dcb0f6157c4c06b11c0f6a02ff181f0
DerZade <DerZade@gmail.com>
2020-02-12 00:31:18 +0100

    Updated js code

--- index.js
+++ index.js
@@ -4,20 +4,26 @@ const exec = require('@actions/exec');
 
 const hemtt = join(__dirname, '../tools', (process.platform === 'linux' ? 'hemtt' : 'hemtt.exe') );
 
-async function execHEMTT(args, options) {
-    try {
-        return exec.exec(hemtt, args, Object.assign({ cwd: '.' }, options || {}) );
-    } catch (err) {
-        core.setFailed(error.message);
+/**
+ * Returns async function, which executes HEMTT with given args and given options
+ * @param {*} args Hemtt args
+ */
+function execHEMTT(args) {
+    return async () => {
+        try {
+            return exec.exec(hemtt, args);
+        } catch (err) {
+            core.setFailed(error.message);
+        }
     }
 };
 
 async function run() {
     // log version
-    await core.group('HEMTT Version', () => execHEMTT(['--version']));
+    await core.group('HEMTT Version', execHEMTT(['--version']));
     
     // build release
-    await core.group('Build mod', () => execHEMTT(['build', '--release', '--force']));
+    await core.group('Build mod', execHEMTT(['build', '--release', '--force']));
     
     let zipName = 'mod';
     // template doesn't seem to work with the current version
@@ -31,7 +37,7 @@ async function run() {
     const zipPath = `./releases/${zipName}.zip`;
             
     // zip
-    await core.group('Zip release', () => execHEMTT(['zip', zipName]));
+    await core.group('Zip release', execHEMTT(['zip', zipName]));
     
     // set outputs
     core.setOutput('zip_name', zipName);
